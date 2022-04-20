cfe99d57e9811ddac957b5ad39adfcfd6df91029
Alexis Guzman <alexis12989x@gmail.com>
2022-04-20 13:00:31 -0500

    feat: add launchx route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (_req, res) => {
   res.send('Hello world!');
 });
 
+app.get('/launchx', (_req, res) => {
+  res.send('Bienvenidos a launchx');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
